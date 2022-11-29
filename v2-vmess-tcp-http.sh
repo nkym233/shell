44ccdcb699e9ea617af2a4284adee1795874ec29
nkym233 <50127423+nkym233@users.noreply.github.com>
2022-11-30 00:27:32 +0800

    Update v2-vmess-tcp-http.sh

--- v2-vmess-tcp-http.sh
+++ v2-vmess-tcp-http.sh
@@ -4,9 +4,9 @@ bash <(curl -L https://git.wej.cc/https://github.com/nkym233/shell/blob/4dd3260f
 sleep 1
 wget -O /usr/local/etc/v2ray/config.json https://git.wej.cc/https://github.com/nkym233/shell/blob/27ec0e8c6f4f98763328cc8fc8d40a2beca2cb5c/config.json
 sleep 1
-read -p "Please enter the port请输入端口：" uport
+read -p "Please enter the port请输入端口并回车：" uport
 echo $uport
-read -p "Please enter the uuid请输入自定义uuid：" uuid
+read -p "Please enter the uuid请输入自定义uuid并回车：" uuid
 echo $uuid
 sed -i "s/uuid/${uuid}/g" /usr/local/etc/v2ray/config.json
 sed -i "s/uport/${uport}/g" /usr/local/etc/v2ray/config.json
@@ -15,7 +15,5 @@ cat /usr/local/etc/v2ray/config.json
 
 systemctl enable v2ray && systemctl restart v2ray
 echo "v2ray已安装并添加到自启"
-sleep 2
-systemctl status v2ray
+sleep 1
 echo "输入systemctl status v2ray，可查看状态"
-
