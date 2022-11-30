8ee308c3d440fabb79eaea30acac0426f48aeeaf
nkym233 <50127423+nkym233@users.noreply.github.com>
2022-11-30 09:41:55 +0800

    Update v2-vmess-tcp-http.sh

--- v2-vmess-tcp-http.sh
+++ v2-vmess-tcp-http.sh
@@ -3,7 +3,7 @@
 # 南柯一梦 WEJ.CC
 bash <(curl -L https://git.wej.cc/https://github.com/nkym233/shell/blob/4dd3260f6d50814adb25edd912786114b54a272d/v2ray-install.sh) --version 4.34.0
 sleep 1
-wget -O /usr/local/etc/v2ray/config.json https://git.wej.cc/https://github.com/nkym233/shell/blob/27ec0e8c6f4f98763328cc8fc8d40a2beca2cb5c/config.json
+wget -O /usr/local/etc/v2ray/config.json --no-check-certificate https://git.wej.cc/https://github.com/nkym233/shell/blob/27ec0e8c6f4f98763328cc8fc8d40a2beca2cb5c/config.json
 sleep 1
 read -p "Please enter the port请输入端口并回车：" uport
 echo $uport
